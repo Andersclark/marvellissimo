e0ad90789559080569308214552cf6664ffd8bb7
Elliott <dahviimakes@gmail.com>
2020-01-16 16:34:54 +0100

    Make picture appear

--- app/src/main/java/com/andersclark/marvellissimo/CharacterDetailActivity.kt
+++ app/src/main/java/com/andersclark/marvellissimo/CharacterDetailActivity.kt
@@ -2,7 +2,9 @@ package com.andersclark.marvellissimo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_character_details.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class CharacterDetailActivity: AppCompatActivity() {
 
@@ -14,10 +16,19 @@ class CharacterDetailActivity: AppCompatActivity() {
             finish()
         }
 
-        val character: MarvelCharacter = intent.getSerializableExtra("character") as MarvelCharacter
+        /*val character: MarvelCharacter = intent.getSerializableExtra("character") as MarvelCharacter
         nameTitle.text = character.name
         description.text=character.description
         //and later add image ref too
+
+         */
+
+        Picasso.get()
+            .load("https://cdn.pixabay.com/photo/2016/11/09/16/24/virus-1812092_1280.jpg")
+            .resize(250, 250)
+            .centerCrop()
+            .placeholder(R.drawable.android_image_1)
+            .into(imageView)
 
     }
 
