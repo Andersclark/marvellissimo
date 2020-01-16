25e3362f133ef387eba4b6f8c7067d42e25faa14
Elliott <dahviimakes@gmail.com>
2020-01-16 13:23:50 +0100

    Getting clicked character to MainActivity

--- app/src/main/java/com/andersclark/marvellissimo/MainActivity.kt
+++ app/src/main/java/com/andersclark/marvellissimo/MainActivity.kt
@@ -3,32 +3,34 @@ package com.andersclark.marvellissimo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), RecyclerAdapter.OnItemClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         createRecyclerView()
-
-        button_showDetails.setOnClickListener {
-            val intent = Intent(this, CharacterDetailActivity::class.java)
-            startActivity(intent)
-        }
     }
 
     private fun createRecyclerView() {
         val layoutManager = LinearLayoutManager(this)
         recycler_view.layoutManager = layoutManager
 
-        val adapter = RecyclerAdapter()
+        val adapter = RecyclerAdapter(this)
         recycler_view.adapter = adapter
     }
+
+    override fun onCharacterItemClicked(character: MarvelCharacter) {
+        Toast.makeText(
+            this,
+            "${character.name} ",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+    
 }
 
--- app/src/main/java/com/andersclark/marvellissimo/RecyclerAdapter.kt
+++ app/src/main/java/com/andersclark/marvellissimo/RecyclerAdapter.kt
@@ -6,9 +6,12 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.snackbar.Snackbar
 
-class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+class RecyclerAdapter (
+   private val itemClickListener: OnItemClickListener
+): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    interface OnItemClickListener { fun onCharacterItemClicked(character: MarvelCharacter)  }
 
     private val thumbnails = intArrayOf(R.drawable.android_image_1,
         R.drawable.android_image_2, R.drawable.android_image_3,
@@ -23,24 +26,23 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
         MarvelCharacter("itemId", "Flash", "Here's a short description of this character", "resourceURI"),
         MarvelCharacter("itemId", "Mr. Fantastic", "Here's a short description of this character", "resourceURI"))
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
 
-        var itemThumbnail: ImageView
-        var itemName: TextView
-        var itemDescription: TextView
+        var itemThumbnail: ImageView = itemView.findViewById(R.id.thumbnail)
+        var itemName: TextView= itemView.findViewById(R.id.name)
+        var itemDescription: TextView=itemView.findViewById(R.id.description)
 
-        init {
-            itemThumbnail = itemView.findViewById(R.id.thumbnail)
-            itemName = itemView.findViewById(R.id.name)
-            itemDescription = itemView.findViewById(R.id.description)
+         fun bind(character: MarvelCharacter) {
+             itemName.text = character.name
+             itemDescription.text = character.description
+             //change to char array eventually
+            // itemThumbnail.setImageResource(thumbnails[i])
 
-            itemView.setOnClickListener { v: View  ->
-                var position: Int = adapterPosition
-
-                Snackbar.make(v, "Clicked guy at position $position",
-                    Snackbar.LENGTH_LONG).setAction("Action", null).show()
-            }
-        }
+             itemView.setOnClickListener {
+                 itemClickListener.onCharacterItemClicked(character)
+             }
+         }
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
@@ -54,9 +56,6 @@ class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
-        viewHolder.itemName.text = testChar[i].name
-        viewHolder.itemDescription.text = testChar[i].description
-        //change to char array eventually
-        viewHolder.itemThumbnail.setImageResource(thumbnails[i])
+        viewHolder.bind(testChar[i])
     }
 }
